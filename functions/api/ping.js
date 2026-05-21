1652aab166c937b3094344daa70cfd4210ed5a09
ORION_Dev <austentube@gmail.com>
2026-05-20 22:21:10 -0500

    Add ping API endpoint for health check

--- functions/api/ping.js
+++ functions/api/ping.js
@@ -1,1 +1,10 @@
-
+export function onRequestGet() {
+  return new Response(JSON.stringify({
+    isOk: true,
+    message: 'PRC-SR API is alive'
+  }), {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}

c660b3df8e1634bd07f8eb4ab5bad41fc22d55df
ORION_Dev <austentube@gmail.com>
2026-07-01 18:29:19 -0500

    Load archive action reliability patch

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -147,6 +147,7 @@ const UI_HEAD_SCRIPTS = [
   '<script src="/js/prc-dash-final-audit.js" defer></script>',
   '<script src="/js/prc-dash-local-bus-edit.js" defer></script>',
   '<script src="/js/prc-dash-print-report.js" defer></script>',
+  '<script src="/js/prc-dash-archive-actions.js" defer></script>',
   '<script src="/js/prc-dash-receiving-summary-window-fix.js" defer></script>',
   '<script src="/js/prc-dash-processing-loaded-summary.js" defer></script>',
   '<script src="/js/prc-dash-current-summary-live-records.js" defer></script>',

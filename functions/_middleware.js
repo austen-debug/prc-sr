b46b35323c61c1af935f1b4faba3cb14be35e593
ORION_Dev <austentube@gmail.com>
2026-07-01 19:21:10 -0500

    style(ui): load input page layout cleanup

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -5,6 +5,7 @@ const UI_STYLESHEETS = [
   '<link rel="stylesheet" href="/css/prc-dash-nav.css">',
   '<link rel="stylesheet" href="/css/prc-dash-states-empty.css">',
   '<link rel="stylesheet" href="/css/prc-dash-watermark.css">',
+  '<link rel="stylesheet" href="/css/prc-dash-input-layout.css">',
   '<link rel="stylesheet" href="/css/prc-dash-danger-actions.css">',
   '<link rel="stylesheet" href="/css/prc-dash-nav-compact.css">'
 ];

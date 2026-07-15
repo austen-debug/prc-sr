e7ef2d9cc3c0582cbb42474060b736431638c865
ORION_Dev <austentube@gmail.com>
2026-07-15 12:51:12 -0500

    Export Build 2 unified shell contracts

---
+++ public/app/shell/index.mjs
@@ -0,0 +1,6 @@
+export * from './route-registry.mjs';
+export * from './permission-registry.mjs';
+export * from './shell-state.mjs';
+export * from './shell-store.mjs';
+export * from './selectors.mjs';
+export * from './renderers.mjs';
